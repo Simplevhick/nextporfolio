ee7690f17c66f0084413ddbebbf47e4ade5dde36
simplevhick <chimborvictor10@gmail.comgit config --global user.email chimborvictor10@gmail.com>
2024-06-24 19:14:57 +0100

    first commit

---
+++ app/resume/page.jsx
@@ -0,0 +1,312 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCcs3,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+  FaCss3,
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNodedotjs, SiNextdotjs } from "react-icons/si";
+
+const about = {
+  title: "About me",
+  description:
+    "As a web developer, I thrive on transforming ideas into engaging digital experiences. With a passion for clean code and user-centric design, I specialize in crafting responsive websites that blend creativity with functionality.",
+  info: [
+    {
+      fieldName: "Name",
+      fieldValue: "Simple Vhick",
+    },
+    {
+      fieldName: "Phone",
+      fieldValue: "(+234) 232 032 54 ",
+    },
+    {
+      fieldName: "Experience",
+      fieldValue: "2+ Years",
+    },
+    {
+      fieldName: "Skype",
+      fieldValue: "Simple_",
+    },
+    {
+      fieldName: "Nationality",
+      fieldValue: "Nigerian",
+    },
+    {
+      fieldName: "Email",
+      fieldValue: "Chimborvictor10@gmail.com",
+    },
+    {
+      fieldName: "Freelance",
+      fieldValue: "Available",
+    },
+    {
+      fieldName: "Languages",
+      fieldValue: "English, Igbo",
+    },
+  ],
+};
+
+const experience = {
+  icon: "/assets/badge.png",
+  title: "My experience",
+  description:
+    "I bring extensive programming expertise to the table, backed by years of successful projects and a deep understanding of various languages and frameworks.",
+  items: [
+    {
+      company: "Spraditech Solutions",
+      position: "Front-End Developer",
+      duration: "2023 - 2024",
+    },
+    {
+      company: "Timeline Entertainment",
+      position: "Front-End Developer Intern",
+      duration: "2024 - present",
+    },
+    {
+      company: "Timeline Entertainment",
+      position: "Freelance Web Developer",
+      duration: "2024 - present",
+    },
+    {
+      company: "Spraditech Solutions",
+      position: "Student",
+      duration: "2023 - 2024",
+    },
+    {
+      company: "Garment Shine",
+      position: "Front-End Developer Intern",
+      duration: "2023 - 2024",
+    },
+  ],
+};
+
+const education = {
+  icon: "/assets/badge.png",
+  title: "My Education",
+  description:
+    "I hold a degree in Web Development from Spraditech Digital Solutions, where i gained solid foundation in programming, etc.",
+  items: [
+    {
+      institution: "Online Course Platform",
+      degree: "UI/UX Bootcamp",
+      duration: "2023",
+    },
+    {
+      institution: "Spraditech Solutions",
+      degree: "Front-end Track",
+      duration: "2023",
+    },
+    {
+      institution: "Online Course",
+      degree: "Programming Course",
+      duration: "2022",
+    },
+    {
+      institution: "Spraditech Solutions",
+      degree: "Certified Front-End Developer",
+      duration: "2024",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "I possess robust skills in web development, with expertise in Css, Tailwind.css, JavaScript, React.js, Next.js, My hands-on experience and commitment to staying current with industry trends ensure I deliver effective solutions that meet client needs.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwind.css",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "node.js",
+    },
+    {
+      icon: <FaFigma />,
+      name: "figma",
+    },
+  ],
+};
+
+import { TabsContent, TabsList, TabsTrigger, Tabs } from "@/components/ui/tabs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
+const Resume = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs
+          defaultValue="experience"
+          className="flex flex-col xl:flex-row gap-[60px]"
+        >
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience">Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About Me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent ">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60 ">{item.company}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="education" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {education.items.map((item, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                        >
+                          <span className="text-accent ">{item.duration}</span>
+                          <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                            {item.degree}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/60 ">{item.institution}</p>
+                          </div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
+              </div>
+            </TabsContent>
+
+            <TabsContent value="skills" className="w-full h-full">
+              <div className="flex flex-col gap-[30px]">
+                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                  <h3 className="text-4xl font-bold">{skills.title}</h3>
+                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                    {skills.description}
+                  </p>
+                </div>
+                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                  {skills.skillList.map((skill, index) => {
+                    return (
+                      <li key={index}>
+                        <TooltipProvider delayDuration={100}>
+                          <Tooltip>
+                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                {skill.icon}
+                              </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              <p className="capitalize">{skill.name}</p>
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+
+            <TabsContent
+              value="about"
+              className="w-full text-center xl:text-left"
+            >
+              <div className="flex flex-col gap-[30px]">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                  {about.info.map((item, index) => {
+                    return (
+                      <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                        <span className="text-white/60">{item.fieldName}</span>
+                        <span className="text-xl">{item.fieldValue}</span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Resume;
---
+++ app/work/page.jsx
@@ -0,0 +1,207 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsBoxArrowInUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
+
+const projects = [
+  {
+    num: "01",
+    category: "React-ecommerce Website",
+    title: "React-ecommerce Website",
+    description:
+      "An innovative React eCommerce project delivering seamless user experience with responsive design.",
+    stack: [
+      {
+        name: "React.js",
+      },
+      {
+        name: "Tailwind.css",
+      },
+      {
+        name: "Javascript",
+      },
+    ],
+    image: "/assets/ecomm.jpg",
+    live: "https://simpleecommerce.vercel.app/",
+    github: "https://github.com/Simplevhick/EcommerceWebsite",
+  },
+  {
+    num: "02",
+    category: "A real-time chat messaging app",
+    title: "project 2",
+    description:
+      "Where real-time communication meets sleek design and seamless performance.",
+    stack: [
+      {
+        name: "Next.js",
+      },
+      {
+        name: "Tailwind",
+      },
+      {
+        name: "Node.js",
+      },
+    ],
+    image: "/assets/chatty.jpg",
+    live: "https://simplechat-six.vercel.app/",
+    github: "https://github.com/Simplevhick/simplechat",
+  },
+  {
+    num: "03",
+    category: "Simpletweet",
+    title: "project 3",
+    description:
+        "Experience the pulse of social media through a seamless interface",
+    stack: [
+      {
+        name: "Next.js",
+      },
+      {
+        name: "Tailwind.css",
+      },
+      {
+        name: "Node.js",
+      },
+    ],
+    image: "/assets/tweet.jpg",
+    live: "https://simpletweet.vercel.app/",
+    github: "https://github.com/Simplevhick/simpletweet",
+  },
+  {
+    num: "04",
+    category: "To-Do List",
+    title: "project 4",
+    description:
+        "typically involves creating and managing a list of tasks and activities that need to be completed within a specific timeframe",
+    stack: [
+      {
+        name: "React.js",
+      },
+      {
+        name: "Css 3",
+      },
+      
+    ],
+    image: "/assets/todo.jpg",
+    live: "https://calm-puppy-c96b5b.netlify.app/",
+    github: "https://github.com/Simplevhick/TodoList",
+  },
+];
+
+const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div className="flex items-center gap-4">
+                <Link href={project.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Live project</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Github repository</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt="pix"
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              <WorkSliderBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none" btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all" />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Work;
